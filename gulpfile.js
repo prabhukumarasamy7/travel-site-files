b463b89d7aa98bcf87a715fdab0c088f525bed1b
Kumarasamy <C997125@hbc.com>
2018-03-07 17:47:31 -0500

    updated the latest css

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default',function() {
 console.log ("WoW - this is your first gulp task!");
@@ -18,8 +19,9 @@ gulp.task("html",function() {
 
 gulp.task('styles',function() {
 	//console.log( 'There are some changes being done in css files');
+	//Note : cssImport package needs to be added at the front of the array list in postcss function
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars,nested,autoprefixer]))
+		.pipe(postcss([cssImport,cssvars,nested,autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
 });
 
